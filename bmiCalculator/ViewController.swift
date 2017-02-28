6b48d63281a2040bdd45f8e21c5da4a3ec8e7990
Maria Hernandez <maximinohernandez@maximinos-mbp.home>
2017-02-28 01:40:39 -0800

    Final touches have been made and additional commenting included!

--- bmiCalculator/ViewController.swift
+++ bmiCalculator/ViewController.swift
@@ -8,43 +8,43 @@
 
 import UIKit
 
+//Function to round to 2 decimal places
+extension Double{
+    func roundTo(places:Int) -> Double{
+        let divisor = pow(10.0, Double(places))
+        return (self*divisor).rounded()/divisor
+    }
+}
+
 class ViewController: UIViewController, UITextFieldDelegate{
     
-    
-
-    //MARK: Properties
+    //MARK: Properties - Labels, TextFields, Images
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var metricUnitsLabel: UILabel!
     @IBOutlet weak var bmiLabel: UILabel!
-    
     @IBOutlet weak var heightTextField: UITextField!
     @IBOutlet weak var weightTextField: UITextField!
-    
     @IBOutlet weak var bmiImage: UIImageView!
-    
     
     //MARK: Variables
     var height:Double!
     var weight:Double!
     var bmi: Double?
-    
     var metricSelect:Bool = false
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        //Delegate the values to self
         self.heightTextField.delegate = self
         self.weightTextField.delegate = self
-        //heightTextField.delegate = height as! UITextFieldDelegate?
-        //weightTextField.delegate = weight as! UITextFieldDelegate?
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     //MARK: UITextFieldDelegate
@@ -53,7 +53,7 @@ class ViewController: UIViewController, UITextFieldDelegate{
         textField.resignFirstResponder()
         return true
     }
-    
+    //Return the keyboard
     func textView(textView: UITextField, shouldChangeTextInRange range: NSRange, replacementText text:String)-> Bool{
         
         if(text == "\n")
@@ -73,69 +73,77 @@ class ViewController: UIViewController, UITextFieldDelegate{
     }
 
     //MARK: Actions
+    //Calculations in Button
     @IBAction func calculateButton(_ sender: UIButton)
     {
-        if metricSelect == false
+        //Conditions to check if in standard or metric
+        if metricSelect == true
         {
-            bmi = (weight!/((height)!*(height)!)).rounded()
+            bmi = (weight!/((height)!*(height)!)).roundTo(places: 2)
         }
-        else
+        else if metricSelect == false
         {
-            bmi = (((weight!)/((height)!*(height!)))*703).rounded()
+            bmi = (((weight!)/((height)!*(height!)))*703).roundTo(places: 2)
         }
         
-        
-        bmiLabel.text = "BMI = \(bmi)"
-        
+        //Conditions under what catagory bmi is under
         if bmi! < 16.00
         {
-            bmiLabel.text = "BMI = \(bmi) Severely Underweight"
+            bmiLabel.text = "BMI = \(bmi!) Severely Underweight"
+            bmiImage.image = UIImage(named:"underweight")
         }
         else if bmi! > 16.00 && bmi! < 16.99
         {
-            bmiLabel.text = "BMI = \(bmi) Moderate Thinness"
+            bmiLabel.text = "BMI = \(bmi!) Moderate Thinness"
+            bmiImage.image = UIImage(named:"underweight")
         }
         else if bmi! > 17.00 && bmi! < 18.49
         {
-            bmiLabel.text = "BMI = \(bmi) Mild Thinness"
+            bmiLabel.text = "BMI = \(bmi!) Mild Thinness"
+            bmiImage.image = UIImage(named:"underweight")
         }
         else if bmi! > 18.50 && bmi! < 24.99
         {
-            bmiLabel.text = "BMI = \(bmi) Normal Range"
+            bmiLabel.text = "BMI = \(bmi!) Normal Range"
+            bmiImage.image = UIImage(named: "normal")
         }
         else if bmi! > 25.0 && bmi! < 29.99
         {
-            bmiLabel.text = "BMI = \(bmi) Overweight"
+            bmiLabel.text = "BMI = \(bmi!) Overweight"
+            bmiImage.image = UIImage(named: "overweight")
         }
         else if bmi! > 30.00 && bmi! < 34.99
         {
-            bmiLabel.text = "BMI = \(bmi) Obese Class I Moderate"
+            bmiLabel.text = "BMI = \(bmi!) Obese Class I Moderate"
+            bmiImage.image = UIImage(named: "overweight")
         }
         else if bmi! > 35.00 && bmi! < 39.99
         {
-            bmiLabel.text = "BMI = \(bmi) Obese Class II Severe"
+            bmiLabel.text = "BMI = \(bmi!) Obese Class II Severe"
+            bmiImage.image = UIImage(named: "obese")
         }
         else if bmi! > 39.99
         {
-            bmiLabel.text = "BMI = \(bmi) Obese Class III Very Severe"
+            bmiLabel.text = "BMI = \(bmi!) Obese Class III Very Severe"
+            bmiImage.image = UIImage(named: "obeseiii")
         }
     }
     
+    //Working the toggle
     @IBAction func metricToggle(_ sender: UISwitch)
     {
         if sender.isOn
         {
-            metricSelect = true
+            metricSelect = false
+            heightTextField.attributedPlaceholder = NSAttributedString(string: "inches")
+            weightTextField.attributedPlaceholder = NSAttributedString(string: "pounds")
             
-            heightTextField.attributedPlaceholder = NSAttributedString(string: "meters")
-            weightTextField.attributedPlaceholder = NSAttributedString(string: "kilograms")
         }
         else
         {
-            metricSelect = false
-            bmiLabel.text = "bmi not in metric"
-            heightTextField.attributedPlaceholder = NSAttributedString(string: "inches")
-            weightTextField.attributedPlaceholder = NSAttributedString(string: "pounds")
+            metricSelect = true
+            heightTextField.attributedPlaceholder = NSAttributedString(string: "meters")
+            weightTextField.attributedPlaceholder = NSAttributedString(string: "kilograms")
             
         }
     }
